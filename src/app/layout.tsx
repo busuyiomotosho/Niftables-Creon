c8ccbf1f6dfc62a1b2ec84487d32db3783dd9d9c
Busuyi Omotosho <busuyiomotosho@yahoo.com>
2024-05-10 02:06:42 +0100

    Added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/Header";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Creon",
